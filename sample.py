4142cc4deb03aab7e0e04238efe4a3c27761733b
Akhil Binu <AkhilBinuRoom@gmail.com>
2023-09-26 13:16:06 +0530

    update

---
+++ sample.py
@@ -0,0 +1,38 @@
+
+number=int(input("Enter a number in between 1 to 7 : "))
+
+def num(number):
+    if(number==1):
+        print("Sunday")
+    elif(number==2):
+        print("monday")
+    elif(number==3):
+        print("tuesday")
+    elif(number==4):
+        print("wedneday")
+    elif(number==5):
+        print("Thurday")
+    elif(number==6):
+        print("Friday")
+    elif(number==7):
+        print("Saturday")
+    else:
+        print("please enter a number between 1 to 7 ")
+
+num(number)
+
+
+# def num_in_words(no):
+#     if(no==1):
+#         print("One")
+#     elif(no==2):
+#         print("Two")
+#     elif(no==3):
+#         print("Three")
+#     else:
+#         print("Give input of numbers from 1 to 3")
+
+    
+# num_in_words(3)
+
+# num_in_words(4.5)
